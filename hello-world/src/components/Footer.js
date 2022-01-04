a08b1fd7da3a2aa8d615ecb87222456d0c8da5e0
Mehak Sadique <mksd1999@gmail.com>
2022-01-03 21:38:15 -0500

    anime project

---
+++ hello-world/src/components/Footer.js
@@ -0,0 +1,17 @@
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+   <div className="footer">
+    
+      <p>Created by: Mehak Sadique</p>
+      
+     
+   </div>
+    
+    </footer>
+  );
+}
+
+export default Footer;
